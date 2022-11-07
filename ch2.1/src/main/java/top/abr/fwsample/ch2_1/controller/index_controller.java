cc0c98e532bf0d38c7612fb1af6cfed4efbac32d
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-11-07 12:39:09 +0800

    Update index_controller.java

--- ch2.1/src/main/java/top/abr/fwsample/ch2_1/controller/index_controller.java
+++ ch2.1/src/main/java/top/abr/fwsample/ch2_1/controller/index_controller.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/") // common prefix of URL
 public class index_controller { // for the requests from index.jsp
-	@RequestMapping("/login")
+	@RequestMapping("login")
 	public String login() {
-		return "login";
+		return "login"; // return view name resolved by view solver
 	}
-	@RequestMapping("/reg")
+	@RequestMapping("reg")
 	public String register() {
 		return "register";
 	}
