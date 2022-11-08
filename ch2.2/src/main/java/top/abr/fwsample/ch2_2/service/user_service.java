e86295bea1cced66148d94466965eee9613d2235
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-11-08 18:40:30 +0800

    /

--- ch2.2/src/main/java/top/abr/fwsample/ch2_2/service/user_service.java
+++ ch2.2/src/main/java/top/abr/fwsample/ch2_2/service/user_service.java
@@ -1,6 +1,8 @@
 package top.abr.fwsample.ch2_2.service;
 
+import top.abr.fwsample.ch2_2.POJO.user_identity;
+
 public interface user_service {
-	boolean login();
-	boolean register();
+	String login(user_identity user);
+	String register(user_identity user);
 }
---
+++ ch2.2/src/main/java/top/abr/fwsample/ch2_2/service/user_service_impl.java
@@ -0,0 +1,14 @@
+package top.abr.fwsample.ch2_2.service;
+
+import top.abr.fwsample.ch2_2.POJO.user_identity;
+
+public class user_service_impl implements user_service {
+	@Override
+	public String login(user_identity user) {
+		return "";
+	}
+	@Override
+	public String register(user_identity user) {
+		return "";
+	}
+}
