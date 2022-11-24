50110d2ba654930f9875111c29b5cc6e367eb57d
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-11-24 16:24:00 +0800

    Update index-add.js

--- ch2.4/src/main/webapp/index-add.js
+++ ch2.4/src/main/webapp/index-add.js
@@ -6,50 +6,37 @@ add_button.onclick = () => {
     console.log(new_speaker_form)
     console.log(new_speaker_form.children)
 
-    const special_action = Object.freeze({
-        comma: 0, lsquare: 1, rsquare: 2, lcurly: 3, rcurly: 4, colon: 5,
-    })
     const s = [ new_speaker_form ]
     let request_body = []
     while (s.length !== 0) {
         const t = s[s.length - 1]
         s.pop()
         if (typeof t !== 'string') {
-            switch (t.tag) {
+            switch (t.tagName.toLowerCase()) {
                 case 'fieldset':
                     const this_is_array = t.getAttribute('title').toLowerCase() === 'map-to-json-array'
-                    // this_is_array ? s.push(special_action.rsquare) : s.push(special_action.rcurly)
                     this_is_array ? s.push(']') : s.push('}')
                     for (const child of t.children) {
-                        s.push(child)
+                        s.push(child, ',')
                     }
-                    // this_is_array ? s.push(special_action.lsquare) : s.push(special_action.lcurly)
-                    this_is_array ? s.push('{') : s.push('{')
+                    this_is_array ? s[s.length - 1] = '{' : s[s.length - 1] = '{'
                     break
-                // case special_action.lsquare:
-                //     request_body.push('[')
-                //     break
-                // case special_action.rsquare:
-                //     request_body.push(']')
-                //     break
-                // case special_action.lcurly:
-                //     request_body.push('{')
-                //     break
-                // case special_action.rcurly:
-                //     request_body.push('}')
-                //     break
-                // case special_action.comma:
-                //     request_body.push('')
-                //     break
                 case 'input':
-                    if (t.getAttribute("type").toLowerCase() === 'checkbox') {
-                        
+                    if (t.getAttribute('type').toLowerCase() === 'checkbox') {
+                        if (t.parentElement.getAttribute("title").toLowerCase() === 'map-to-json-array') {
+                            s.push(t.getAttribute('name'));
+                        } else {
+                            s.push(t.getAttribute('checked'), ':', t.getAttribute('name'))
+                        }
                     } else {
-
+                        {
+                            const next_node = t.nextElementSibling
+                            if (next_node.tagName.toLowerCase() === 'label') {
+                                s.push(next_node.textContent, ':', 'unit', ',')
+                            }
+                        }
+                        s.push(t.getAttribute('value'), ':', t.getAttribute('name'))
                     }
-                    break
-                case 'label':
-
                     break
                 default:
                     break
@@ -58,4 +45,6 @@ add_button.onclick = () => {
             request_body.push(t)
         }
     }
+    request_body.join('')
+    console.log(request_body)
 }
