2e4ae23cf6d42d980a621ac53adeff716884e9cf
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-11-24 14:55:06 +0800

    Update index-add.js

--- ch2.4/src/main/webapp/index-add.js
+++ ch2.4/src/main/webapp/index-add.js
@@ -6,14 +6,25 @@ add_button.onclick = () => {
     console.log(new_speaker_form)
     console.log(new_speaker_form.children)
 
-    const s = [new_speaker_form]
-    const r = {}
-    const m = {s: r}
+    const special_action = Object.freeze({
+        comma: 0, lsquare: 1, rsquare: 2, lcurly: 3, rcurly: 4, colon: 5,
+    })
+    const s = [ new_speaker_form ]
+    const actions = []
+    const request_body = ""
     while (s.length !== 0) {
         const t = s[s.length - 1]
         s.pop()
         switch (t.tag) {
             case 'fieldset':
+                actions.push(special_action.lcurly)
+                let checkbox_only = true
+                for (const child of t.children) {
+                    s.push(child);
+                    if (child.tagName.toLowerCase() === 'input' && child.getAttribute("type") !== "checkbox") {
+                        checkbox_only = false
+                    }
+                }
 
                 break
             case 'input':
