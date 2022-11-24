89109cba0a68364c2be1f0343aa1d14939143dd8
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-11-24 21:47:23 +0800

    Update index-add2.js

--- ch2.4/src/main/webapp/index-add2.js
+++ ch2.4/src/main/webapp/index-add2.js
@@ -2,8 +2,6 @@ const add_button = document.getElementById("add_button")
 add_button.onclick = () => {
     const speaker = {}
     const new_speaker_fieldset = document.getElementById("new-speaker")
-    console.log(Date.now())
-
     speaker['model'] = new_speaker_fieldset.querySelector('input[name="model"]').getAttribute('value')
     {
         speaker['type'] = []
@@ -14,7 +12,65 @@ add_button.onclick = () => {
         }
     }
     {
-        driver_fieldset = new_speaker_fieldset.querySelector('fieldset[name="driver"]')
-        
+        const driver_fieldsets = new_speaker_fieldset.querySelectorAll('fieldset[name="driver"]>fieldset')
+        for (const fieldset of driver_fieldsets) {
+            const direct_inputs = fieldset.querySelectorAll('input.parent>input')
+            {
+                let no_content = true
+                for (const input of direct_inputs) {
+                    if (input.getAttribute('value') !== null) {
+                        no_content = false
+                        break
+                    }
+                }
+                if (no_content) continue
+            }
+            const driver_type = fieldset.getAttribute('name')
+            for (const input of direct_inputs) {
+                const value = input.getAttribute('value')
+                const unit_label = input.nextElementSibling
+                const target = speaker['driver']
+                if (unit_label === null) {
+                    if (value !== null) target[driver_type] = value
+                } else {
+                    if (value !== null) target[driver_type] = {
+                        value: value, unit: unit_label.textContent
+                    }
+                }
+            }
+            const power_fieldset = fieldset.querySelector('fieldset[name="power"]')
+            const power_inputs = power_fieldset.querySelectorAll('input')
+            for (const input of power_inputs) {
+                const value = input.getAttribute('value')
+                const target = speaker['driver'][driver_type]
+                if (value !== null) target[input.getAttribute('name')] = {
+                    value: value, unit: input.nextElementSibling.textContent
+                }
+            }
+        }
     }
+    {
+        const frequency_response_fieldset = new_speaker_fieldset.querySelector('fieldset[name="frequency_response"]')
+        const dimension_fieldset = new_speaker_fieldset.querySelector('fieldset[name="dimension"]')
+        const fieldsets = [ frequency_response_fieldset, dimension_fieldset, ]
+        for (const fieldset of fieldsets) {
+            const inputs = frequency_response_fieldset.querySelectorAll('input')
+            for (const input of inputs) {
+                const value = input.getAttribute('value')
+                const unit_label = input.nextElementSibling
+                const target = speaker['frequency_response']
+                if (value !== null) target[input.getAttribute('name')] = {
+                    value: value, unit: unit_label.textContent
+                }
+            }
+        }
+    }
+    {
+        const weight_input = new_speaker_fieldset.querySelector('input[name="weight"]')
+        const weight_unit = weight_input.nextElementSibling.textContent
+        speaker['weight'] = {
+            value: weight_input.getAttribute('value'), unit: weight_unit
+        }
+    }
+    console.log(JSON.stringify(speaker))
 }
