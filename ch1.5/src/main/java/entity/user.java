e13de625586b398c627717a8147b4b348e01a860
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-09 13:51:06 +0800

    /

--- ch1.5/src/main/java/entity/user.java
+++ ch1.5/src/main/java/entity/user.java
@@ -4,6 +4,12 @@ public class user {
 	Long id;
 	String name;
 	String sex;
+	public Long getId() {return id;}
+	public void setId(Long id) {this.id = id;}
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+	public String getSex() {return sex;}
+	public void setSex(String sex) {this.sex = sex;}
 	public String toString() {
 		return "user[id = " + id + ", name = " + name + ", sex = " + sex + "]";
 	}
