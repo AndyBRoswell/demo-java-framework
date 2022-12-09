1895e27c74856877c0dd32eedfe8cde710d0d6b4
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-09 13:59:59 +0800

    Update service_impl.java

--- ch1.5/src/main/java/service/service_impl.java
+++ ch1.5/src/main/java/service/service_impl.java
@@ -17,11 +17,13 @@ public class service_impl implements service {
 	public void test_JDBC() {
 		final var insert_SQL = "insert into spring_test.user values(?,?,?)";
 		final var select_SQL = "select * from spring_test.user";
+		final var truncate_SQL = "truncate table spring_test.user";
 		final var random = new Random(Instant.now().getEpochSecond());
 		for (long i = 1; i <= 10; ++i) {
 			test_DAO.update(insert_SQL, new Object[]{ i, "老王" + random.nextLong(), random.nextBoolean() == true ? "男" : "女", });
-			List<user> user_list = test_DAO.query(select_SQL, null);
-			for (var user : user_list) {System.out.println(user);}
 		}
+		List<user> user_list = test_DAO.query(select_SQL, null);
+		for (var user : user_list) {System.out.println(user);}
+		test_DAO.update(truncate_SQL, null);
 	}
 }
