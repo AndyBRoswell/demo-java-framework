88a8f7bb4400368a5aadf50530efe289673b50b0
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-10 23:47:46 +0800

    Update test.java

--- ch3.1/src/test/java/test.java
+++ ch3.1/src/test/java/test.java
@@ -21,13 +21,13 @@ public class test {
 			final SqlSession SQL_session = SQL_session_factory.openSession();
 			final var mybatis_prefix = "mybatis_mapper.user-mapper.";
 			final List<user> expected_users = new ArrayList<>();
-			final long n = 100L;
+			final long n = 10L;
 			final var random = new Random(Instant.now().getEpochSecond());
 			for (var i = 0; i < n; ++i) {
 				final user user = new user();
 				user.setId(random.nextLong());
 				final StringBuilder name_string_builder = new StringBuilder();
-				final int l = random.nextInt(1, 8);
+				final int l = random.nextInt(1, 32);
 				for (var j = 0; j < l; ++j) {name_string_builder.append((char)random.nextInt(0x20, 0x7E));}
 				user.setName(name_string_builder.toString());
 				user.setSex(random.nextBoolean() == true ? "M" : "F");
