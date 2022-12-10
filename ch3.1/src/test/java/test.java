10c67243d4bdf53c29c3802e9c0c91ed0ed6ede0
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-10 22:54:46 +0800

    Update test.java

--- ch3.1/src/test/java/test.java
+++ ch3.1/src/test/java/test.java
@@ -2,6 +2,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
 import persistence.user;
 
 import java.io.IOException;
@@ -12,8 +13,11 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class test {
-	public static void main(String[] args) {
+	@Test
+	public static void MyBatis_demo() {
 		try {
 			final InputStream config = Resources.getResourceAsStream("mybatis-config.xml");
 			final SqlSessionFactory SQL_session_factory = new SqlSessionFactoryBuilder().build(config);
@@ -22,24 +26,24 @@ public class test {
 			final Set<user> control_group = new HashSet<>();
 			final long n = 100L;
 			final var random = new Random(Instant.now().getEpochSecond());
-			for (var i = 1; i <= n; ++i) {
-				final user u = new user();
-				u.setId(random.nextLong());
+			for (var i = 0; i < n; ++i) {
+				final user user = new user();
+				user.setId(random.nextLong());
 				final StringBuilder name_string_builder = new StringBuilder();
 				final int l = random.nextInt(1, 32);
 				for (var j = 0; j < l; ++j) {name_string_builder.append(random.nextInt(0x20, 0x7E));}
-				u.setName(name_string_builder.toString());
-				u.setSex(random.nextBoolean() == true ? "M" : "F");
-				control_group.add(u);
-				SQL_session.insert(mybatis_prefix + "add_user");
+				user.setName(name_string_builder.toString());
+				user.setSex(random.nextBoolean() == true ? "M" : "F");
+				control_group.add(user);
+				SQL_session.insert(mybatis_prefix + "add_user", user);
 			}
-			final user user_selected = SQL_session.selectOne("mybatis_mapper.user-mapper.select_user_by_id", 1L);
-			System.out.println(user_selected);
-			final user user_to_be_added = new user();
-			user_to_be_added.setId(1L);
-			user_to_be_added.setName("小王");
-			user_to_be_added.setSex("男");
-			SQL_session.insert("mybatis_mapper.user-mapper.add_user", user_to_be_added);
+			for (var expected_user : control_group) {
+				final user returned_user = SQL_session.selectOne("mybatis_mapper.user-mapper.select_user_by_id", expected_user.getId());
+				assertEquals(returned_user, expected_user);
+			}
+			for (var i = 0; i < n; ++i) {
+				
+			}
 			final user user_to_be_updated = new user();
 			user_to_be_updated.setId(1L);
 			user_to_be_updated.setName("老张");
