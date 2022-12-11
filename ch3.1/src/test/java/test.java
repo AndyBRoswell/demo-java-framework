ecb94674d45b2a2c47aa7ab4343aca4ff7ac0123
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-11 11:10:56 +0800

    /

--- ch3.1/src/test/java/test.java
+++ ch3.1/src/test/java/test.java
@@ -21,8 +21,9 @@ public class test {
 			final SqlSession SQL_session = SQL_session_factory.openSession();
 			final var mybatis_prefix = "mybatis_mapper.user-mapper.";
 			final List<user> expected_users = new ArrayList<>();
-			final long n = 10L;
+			final int n = 10;
 			final var random = new Random(Instant.now().getEpochSecond());
+			// insert
 			for (var i = 0; i < n; ++i) {
 				final user user = new user();
 				user.setId(random.nextLong());
@@ -34,11 +35,13 @@ public class test {
 				expected_users.add(user);
 				SQL_session.insert(mybatis_prefix + "add_user", user);
 			}
+			assertEquals(n, expected_users.size());
+			// select
 			for (var expected_user : expected_users) {
 				final user actual_user = SQL_session.selectOne(mybatis_prefix + "select_user_by_id", expected_user.getId());
 				assertTrue(expected_user.equals(actual_user));
 			}
-			SQL_session.commit();
+			// update
 			for (var user : expected_users) {
 				final StringBuilder name_string_builder = new StringBuilder();
 				final int l = random.nextInt(1, 32);
@@ -47,14 +50,20 @@ public class test {
 				user.setSex(random.nextBoolean() == true ? "M" : "F");
 				SQL_session.update(mybatis_prefix + "update_user", user);
 			}
-			SQL_session.commit();
+			// select
 			final List<user> actual_users = SQL_session.selectList(mybatis_prefix + "select_all_users");
 			final var user_comparator = Comparator.comparingLong(user::getId).thenComparing(user::getName).thenComparing(user::getSex);
 			expected_users.sort(user_comparator);
 			actual_users.sort(user_comparator);
-			assertEquals(expected_users.size(), actual_users.size());
-			for (var i = 0; i < expected_users.size(); ++i) {
+			assertEquals(n, actual_users.size());
+			for (var i = 0; i < n; ++i) {
 				assertTrue(expected_users.get(i).equals(actual_users.get(i)));
+			}
+			// delete
+			Collections.shuffle(expected_users);
+			for (var i = n - 1; i >= n / 2; --i) {
+				SQL_session.delete(mybatis_prefix + "delete_user", expected_users.get(i).getId());
+				expected_users.remove(i);
 			}
 			SQL_session.commit();
 			SQL_session.close();
