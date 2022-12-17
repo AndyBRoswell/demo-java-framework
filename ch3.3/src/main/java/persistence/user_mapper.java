d5d824fefb067e5758bfec6976a067c3d16da792
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-17 22:02:23 +0800

    /

--- ch3.3/src/main/java/persistence/user_mapper.java
+++ ch3.3/src/main/java/persistence/user_mapper.java
@@ -14,5 +14,5 @@ public interface user_mapper {
 	int clear_users();
 	List<user> MySQL_select_users_by_map(Map<String, Object> param);
 	List<user> MySQL_select_users_by_bean(user user);
-	List<user> MySQL_select_users_by_param_annotation(@Param("p_name") String name, @Param("p_sex") String sex);
+	List<user> MySQL_select_users_by_param_annotation(@Param("name") String name, @Param("sex") String sex);
 }
