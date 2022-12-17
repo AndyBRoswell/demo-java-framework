2f966888f4324a5009e150fa1a7499a7e09505e8
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-17 19:53:39 +0800

    /

--- ch3.3/src/main/webapp/index.js
+++ ch3.3/src/main/webapp/index.js
@@ -1,11 +1,12 @@
 const response_area = document.getElementById('response')
-const test_button = document.getElementById('test')
-const show_response_body = () => {
-    const HTTP_request = new XMLHttpRequest()
-    HTTP_request.open('get', 'test', true)
-    HTTP_request.send()
-    HTTP_request.onloadend = () => {
-        response_area.textContent = HTTP_request.responseText
+const inputs = document.getElementsByTagName('input')
+for (const input of inputs) {
+    input.onclick = () => {
+        const HTTP_request = new XMLHttpRequest()
+        HTTP_request.open('get', input.getAttribute('value'), true)
+        HTTP_request.send()
+        HTTP_request.onloadend = () => {
+            response_area.textContent = HTTP_request.responseText
+        }
     }
 }
-test_button.onclick = show_response_body
--- ch3.3/src/main/webapp/index.jsp
+++ ch3.3/src/main/webapp/index.jsp
@@ -6,8 +6,8 @@
     <script src="index.js" defer></script>
 </head>
 <body>
-<input type="button" id="test" value="test"><br>
-<a href="test_MySQL_select_user_by_map">MySQL_select_user_by_map</a>
+<input type="button" value="test"><br>
+<input type="button" value="test_MySQL_select_user_by_map"><br>
 <textarea id="response" style="height: 90vh; width: 100%" readonly></textarea>
 </body>
 </html>
