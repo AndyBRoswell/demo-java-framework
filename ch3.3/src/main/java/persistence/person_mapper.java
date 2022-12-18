a56b7beeba4395c7c2f2d49a7584fec8e25a3c01
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-12-18 16:42:20 +0800

    /

--- ch3.3/src/main/java/persistence/person_mapper.java
+++ ch3.3/src/main/java/persistence/person_mapper.java
@@ -1,8 +1,11 @@
 package persistence;
 
+import java.util.List;
+
 public interface person_mapper {
 	person select_person_by_id_1(final Long id);
 	person select_person_by_id_2(final Long id);
+	List<person> select_all_persons();
 	int add_person(final person person);
 	int add_person_with_auto_increment(final person person);
 	int update_person(final person person);
--- ch3.3/src/main/java/persistence/person_mapper_impl.java
+++ ch3.3/src/main/java/persistence/person_mapper_impl.java
@@ -1,18 +1,25 @@
 package persistence;
 
+import java.util.List;
+
 public class person_mapper_impl extends base_mapper implements person_mapper {
+	private static final String MyBatis_prefix = "persistence.person_mapper.";
 
 	@Override
 	public person select_person_by_id_1(Long id) {
-		return null;
+		return getSqlSession().selectOne(MyBatis_prefix + "select_person_by_id_1");
 	}
 	@Override
 	public person select_person_by_id_2(Long id) {
-		return null;
+		return getSqlSession().selectOne(MyBatis_prefix + "select_person_by_id_2");
+	}
+	@Override
+	public List<person> select_all_persons() {
+		return getSqlSession().selectList(MyBatis_prefix + "select_all_persons");
 	}
 	@Override
 	public int add_person(person person) {
-		return 0;
+		return getSqlSession().insert(MyBatis_prefix + "");
 	}
 	@Override
 	public int add_person_with_auto_increment(person person) {
